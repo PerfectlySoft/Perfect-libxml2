8c8e026df53d9c42db0ccc186dad6e28aedda574
Kyle Jessup <badthing@gmail.com>
2016-08-19 11:17:51 -0400

    Corrected include path

--- libxml2.h
+++ libxml2.h
@@ -3,6 +3,6 @@
 #define _libxml2_h_
 #include <libxml2/libxml/tree.h>
 #include <libxml2/libxml/xpath.h>
-#include <libxml2/libxml/xpathinternals.h>
+#include <libxml2/libxml/xpathInternals.h>
 #endif
 
