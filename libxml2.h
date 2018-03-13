7fac51dc830bfc92814daa8d9aa9aa3b6e61ba03
Kyle Jessup <kyle@perfect.org>
2018-03-13 17:08:31 -0400

    Update libxml2.h

--- libxml2.h
+++ libxml2.h
@@ -7,6 +7,7 @@
 #include <libxml2/libxml/xpathInternals.h>
 #include <libxml2/libxml/HTMLparser.h>
 #include <libxml2/libxml/parser.h>
+#include <libxml2/libxml/entities.h>
 #include <libxml2/libxml/SAX.h>
 #include <libxml2/libxml/SAX2.h>
 #endif
